703469cd767c39db03653bb58c561c5a30b9a16e
Davi <davimss@gmail.com>
2019-07-19 15:38:33 -0300

    Aula 9.3 concluída

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
+import { BemVindo2Component } from './bem-vindo2/bem-vindo2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    BemVindoComponent,
+    BemVindo2Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/bem-vindo2/bem-vindo2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bem-vindo2',
+  templateUrl: './bem-vindo2.component.html',
+  styleUrls: ['./bem-vindo2.component.css']
+})
+export class BemVindo2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
