9a06285b6224d7fb5d2fd71554af2f6ce2c64755
Davi <davimss@gmail.com>
2019-08-10 11:05:07 -0300

    Aula 9.16 concluída

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-funcionario-card',
   templateUrl: './funcionario-card.component.html',
-  styleUrls: ['./funcionario-card.component.css']
+  styleUrls: ['./funcionario-card.component.css'],
+  styles: [`
+  * {
+  font-size: 25px;
+  font-family: "Open Sans"
+  }
+  `]
+
 })
 export class FuncionarioCardComponent {
 
