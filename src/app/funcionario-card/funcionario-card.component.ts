347fc0c6c946ace29a43a01502938a608e576a57
Davi <davimss@gmail.com>
2019-08-10 11:13:08 -0300

    Aula 9.17 concluída

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -16,4 +16,14 @@ export class FuncionarioCardComponent {
 
   @Input('obj') funcionario: any;
 
+  getEstilosCartao() {
+    return {
+      'border-width.px': this.funcionario.id,
+      backgroundColor: this.funcionario.id % 2 === 0 ? 'lightblue' : 'lightgreen'
+
+    };
+  }
+
+
+
 }
